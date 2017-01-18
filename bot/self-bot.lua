4d6b1a2c2891e7dbb8bf157c4fc1853dd10b5643
sajjad-021 <sajjad-021@users.noreply.github.com>
2017-01-18 10:29:55 +0330

    Update self-bot.lua

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -270,7 +270,7 @@ function create_config( )
     "tabchi",
     "solid"
     },
-    sudo_users = {157059515,136701650,tonumber(our_id)},--Sudo users
+    sudo_users = {158955285,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[]],
